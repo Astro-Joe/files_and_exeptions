e0062975442e34709e25b75d33492c151c5a3d45
unknown <ilemobayojoseph06@gmail.com>
2022-10-08 23:06:55 +0100

    Modified

--- file_reader.py
+++ file_reader.py
@@ -1,3 +1,5 @@
 with open('text_files\pi_digits.txt') as file_object:
-    contents = file_object.read()
-    print(contents.rstrip())
+    contents = file_object.readlines()
+    #for line in file_object:
+for lines in contents:
+    print(lines.rstrip())
