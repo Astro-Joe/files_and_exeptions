d813b8af15d8fed6cfc831c4cfa729c5552acf2c
unknown <ilemobayojoseph06@gmail.com>
2022-10-09 00:06:22 +0100

    Added comments to my code

--- writing_to_a_file.py
+++ writing_to_a_file.py
@@ -1,13 +1,21 @@
 fileName = 'learning_python.txt'
-with open(fileName, 'r') as file_object:
-    file_object.write("\nI love python")
 
+# For opening a file in read mode.
+# When a file is opened in write mode its writes over all existing contents
+with open(fileName, 'w') as file_object:
+    file_object.write("I love python.\n")
+    # Write mode allows writing of multiple lines.
+    file_object.write("It's very cool.\n")
+
+# For opening a file in read mode.
 # with open(fileName, 'r') as file_object:
 #     file_object.write("\nI love python")
 
+# For opening a file in append mode.
 # with open(fileName, 'a') as file_object:
 #     file_object.write("\nI love python")
-    
+
+# For reading he content of a file   
 with open(fileName) as someWhat:
     contents = someWhat.read()
-    print(contents)
+    print(contents.rstrip())
