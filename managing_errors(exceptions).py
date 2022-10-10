2dd8ef1a1173a9113070e3431eaeb495afc8d62e
unknown <ilemobayojoseph06@gmail.com>
2022-10-10 22:12:05 +0100

    Modified

--- managing_errors(exceptions).py
+++ managing_errors(exceptions).py
@@ -1,13 +1,20 @@
 while True:
+    print("Enter 'q' to quit")
     prompt = "Enter a number: "
     number_ = input(prompt)
+    if number_ == 'q':
+        break
     number1  = input(prompt)
+    if number1 == 'q':
+        break
     
-   
+   # The except block can be used as much as possible
     try:
         solve = int(number1) / int(number_)
-        print(solve)
+        
     except ZeroDivisionError:
         print("You can't divide by zero!\n")
     except ValueError:
         print('Numbers! not letters!\n')
+    else:
+        print(str(solve) + '\n')
--- writing_to_a_file.py
+++ writing_to_a_file.py
@@ -2,18 +2,19 @@ fileName = 'learning_python.txt'
 
 # For opening a file in read mode.
 # When a file is opened in write mode its writes over all existing contents
-with open(fileName, 'w') as file_object:
-    file_object.write("I love python.\n")
-    # Write mode allows writing of multiple lines.
-    file_object.write("It's very cool.\n")
+# with open(fileName, 'w') as file_object:
+#     file_object.write("I love python.\n")
+#     # Write mode allows writing of multiple lines.
+#     file_object.write("It's very cool.\n")
 
 # For opening a file in read mode.
 # with open(fileName, 'r') as file_object:
 #     file_object.write("\nI love python")
 
-# For opening a file in append mode.
-# with open(fileName, 'a') as file_object:
-#     file_object.write("\nI love python")
+#For opening a file in append mode.
+with open(fileName, 'a') as file_object:
+    file_object.write("I also love finding meaning in large dataset.\n")
+    file_object.write("I love creating apps that can run in a browser.\n")
 
 # For reading he content of a file   
 with open(fileName) as someWhat:
