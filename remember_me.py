efd7db92103eb5e6b4a602730bb1648598ef8d90
unknown <ilemobayojoseph06@gmail.com>
2022-10-12 23:16:53 +0100

    Modified the function greet_user()

--- remember_me.py
+++ remember_me.py
@@ -9,7 +9,7 @@ def get_store_username():
     except FileNotFoundError:
         return None
     else:
-        return username
+        return username.title()
 
 
 def get_new_username():
@@ -18,16 +18,19 @@ def get_new_username():
     filename = 'username.json'
     with open(filename, 'w') as f_obj:
         json.dump(username, f_obj)
-    return username
+    return username.title()
 
 def greet_user():
     """Greet the user by name"""
     username = get_store_username()
     if username:
-        print("Welcome back, " + username + "!")
-    else:
-        username = get_new_username()
-        print("We'll remember you when you come back, " + username + "!")
+        print("Is " + username + " your username.")
+        response = input("Enter yes/no: ")
+        if response.lower() == "yes":
+            print("Welcome back, " + username)
+        elif response.lower() == "no":
+            username = get_new_username()
+            print("We'll remember you when you come back, " + username + "!")
 
 
 greet_user()
